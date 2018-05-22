64251765ed409d434f25573441cf92852be51726
Steve <spdevuk@gmail.com>
2018-05-23 00:21:47 +0100

    ejected and added css modules to config, removed everythin gfrom app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
 
-import Person from './Person';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.doSomething = this.doSomething.bind(this);
-    this.state = {
-      text: ['One', 'Two', 'Three', 'Four']
-    };
-  }
 
-  doSomething() {
-    const arr = this.state.text.map(this.addEnd);
-    console.log(...arr);
-  }
-  addEnd = str => {
-    const finished = `${str}..!!!!!`;
-    return finished;
-  };
+
 
   render() {
     return (
       <div className="App">
-        <h1 onClick={this.doSomething}>hi</h1>
+        <h1>hi</h1>
       </div>
     );
   }
