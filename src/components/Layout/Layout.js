ef8d3e3f575cfd8b50b8d27729207e7356dbd209
Steve <spdevuk@gmail.com>
2018-05-26 23:07:49 +0100

    Added toolbar / navbar + Logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactAux from '../../hoc/ReactAux';
 
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = props => (
   <ReactAux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </ReactAux>
 );
