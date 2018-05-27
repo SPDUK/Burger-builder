fdda4885abb29696d1908314112905ec074d7684
Steve <spdevuk@gmail.com>
2018-05-27 21:50:29 +0100

    added menu click to display sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,17 +6,21 @@ import classes from './Layout.css';
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   };
 
   SideDrawerClosedHandler = () => {
     this.setState({ showSideDrawer: false });
   };
 
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => ({ showSideDrawer: !prevState.showSideDrawer }));
+  };
+
   render() {
     return (
       <ReactAux>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.SideDrawerClosedHandler}
--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const drawerToggle = (props) => (
-
+const drawerToggle = props => (
+  <div
+    onKeyDown={props.clicked}
+    role="button"
+    tabIndex="0"
+    onClick={props.clicked}
+  >
+    MENU
+  </div>
 );
 
 export default drawerToggle;
